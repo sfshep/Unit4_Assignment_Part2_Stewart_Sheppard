a0f4a215d436dbff17b7cf5a0dc4330fc5eab533
Fred Sheppard <stewart.sheppard@my.tccd.edu>
2020-02-09 06:45:45 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+Stewart Sheppard
+Unit4_Assignment- Part 2 - Loops (Repetition Structures)
+COSC 1436 570002
+Repl.it / Github
+This program will work with counters and perform calculations.
+*/
+#include <cstdlib>  // Provides EXIT_SUCCESS
+#include <iostream>  // Provides cout, cerr, endl
+#include "Input_Validation_Extended.h" // Provides getValidatedInput
+#include<string> //for string manipulation
+#include<sstream> //for string manipulation and comparison
+
+using namespace std;
+
+int main() 
+{
+
+}
